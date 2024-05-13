82fb49520ab859da844f529fee68058cc5e83627
Jaz-3-0 <sebejaz99@gmail.com>
2024-05-13 21:49:51 +0200

    update init clarify

--- token/CreateAsset.js
+++ token/CreateAsset.js
@@ -11,12 +11,17 @@ const client = new algosdk.Algodv2(token, server, port);
 // deploy token
 const deployToken = async() => {
     
+      // Read the account details from JSON
     const accountData = JSON.parse(fs.readFileSync('account.json', 'utf-8'));
     const { address, privateKey } = accountData;
+
+      // Convert the private key from base64 string back to Uint8Array
     const privateKeyUint8 = new Uint8Array(Buffer.from(privateKey, "base64"));
 
+      // Get suggested transaction parameters
     const suggestedParams = await client.getTransactionParams().do();
 
+      // Create an asset creation transaction
     console.log("Creating token Metadata....");
     const txn = algosdk.makeAssetCreateTxnWithSuggestedParams({
         from: address,
@@ -32,10 +37,14 @@ const deployToken = async() => {
         decimals: 0, // decimals
     })
 
+      // Sign the transaction
     const signedTxn = algosdk.signTransaction(txn, privateKeyUint8);
 
-    await algodClient.sendRawTransaction(signedTxn.blob).do();
+      // Submit the transaction to the network
 
+    await client.sendRawTransaction(signedTxn.blob).do();
+
+    // Wait for confirmation
     const result = await algosdk.waitForConfirmation(algodClient, txn.txID().toString(), 3);
 
     console.log("Token deployed successfully!!");
@@ -43,4 +52,13 @@ const deployToken = async() => {
     const assetIndex = result['asset-index'];
 
     console.log(`Asset ID created: ${assetIndex}`);
+    
+    // Display AlgoExplorer URL
+    const url = `https://testnet.explorer.perawallet.app/asset/${assetIndex}`;
+    console.log(`Asset Url: ${url}`);
+
+    // End the console
+    process.exit();
 }
+
+deployToken().catch(console.error);
